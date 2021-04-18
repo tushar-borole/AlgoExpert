2c191f8f73080e1252e172c958ecaa4c818e9edb
tusharborole <tushariscoolster@gmail.com>
2021-04-17 20:47:36 -0700

    1st program

---
+++ twoNumberSum.js
@@ -0,0 +1,15 @@
+function twoNumberSum(array, targetSum) {
+    for(let i = 0; i < array.length; i++){
+        for(let j = i+1; j < array.length; j++){
+            if(array[i] + array[j] === targetSum){
+                return [array[i],array[j]]
+            }
+        }
+    }
+
+    return  []
+}
+
+
+twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6],10) //?
+
